59348f55444d48744ad9c533dc0c3953478f8fe4
IdanK <me@idank.dev>
2026-08-01 11:40:52 +0300

    Typo fix

--- internal/terminal/state.go
+++ internal/terminal/state.go
@@ -7,10 +7,10 @@ import (
 )
 
 type ShellState struct {
-    Fd int
-    OldState *term.State
-    CommandHistory []string
-    HistoryIndex int
+	Fd             int
+	OldState       *term.State
+	CommandHistory []string
+	HistoryIndex   int
 }
 
 func NewShellState() (*ShellState, error) {
@@ -20,45 +20,44 @@ func NewShellState() (*ShellState, error) {
 		return nil, err
 	}
 
-    return &ShellState{
-        Fd: fd,
-        OldState: oldState,
-        CommandHistory: make([]string, 0),
-    }, nil
+	return &ShellState{
+		Fd:             fd,
+		OldState:       oldState,
+		CommandHistory: make([]string, 0),
+	}, nil
 }
 
 func (state *ShellState) Restore() error {
-    state.HistoryIndex = 0
-    clear(state.CommandHistory)
-    return term.Restore(state.Fd, state.OldState)
+	state.HistoryIndex = 0
+	clear(state.CommandHistory)
+	return term.Restore(state.Fd, state.OldState)
 }
 
 func (state *ShellState) AddHistory(cmd string) {
-    if cmd == "" {
-        return
-    }
+	if cmd == "" {
+		return
+	}
 
-    state.CommandHistory = append(state.CommandHistory, cmd)
+	state.CommandHistory = append(state.CommandHistory, cmd)
 }
 
-func (state *ShellState) Move(diraction rune) string {
-    historyLen := len(state.CommandHistory)
-    if historyLen == 0 {
-        return ""
-    }
+func (state *ShellState) Move(direction rune) string {
+	historyLen := len(state.CommandHistory)
+	if historyLen == 0 {
+		return ""
+	}
 
-    if diraction == Up {
-        state.HistoryIndex += 1
-        if state.HistoryIndex >= historyLen {
-            state.HistoryIndex = historyLen - 1
-        }
-    } else if diraction == Down {
-        state.HistoryIndex -= 1
-        if state.HistoryIndex < 0 {
-            state.HistoryIndex = 0
-        }
-    }
+	if direction == Up {
+		state.HistoryIndex += 1
+		if state.HistoryIndex >= historyLen {
+			state.HistoryIndex = historyLen - 1
+		}
+	} else if direction == Down {
+		state.HistoryIndex -= 1
+		if state.HistoryIndex < 0 {
+			state.HistoryIndex = 0
+		}
+	}
 
-    return state.CommandHistory[state.HistoryIndex]
+	return state.CommandHistory[state.HistoryIndex]
 }
-
